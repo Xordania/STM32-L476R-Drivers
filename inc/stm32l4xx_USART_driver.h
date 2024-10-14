cf31719e2d1f2a7ae456ac6e2f5817025e4d4f15
JFearn <jordanfearn2015@gmail.com>
2024-10-14 20:08:03 +0100

    Created USART files

---
+++ inc/stm32l4xx_USART_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32l4xx_USART_driver.h
+ *
+ *  Created on: 14 Oct 2024
+ *      Author: Pillow sleeper
+ */
+
+#ifndef STM32_L476R_DRIVERS_INC_STM32L4XX_USART_DRIVER_H_
+#define STM32_L476R_DRIVERS_INC_STM32L4XX_USART_DRIVER_H_
+
+
+
+#endif /* STM32_L476R_DRIVERS_INC_STM32L4XX_USART_DRIVER_H_ */
---
+++ src/stm32l4xx_USART_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32l4xx_USART_driver.c
+ *
+ *  Created on: 14 Oct 2024
+ *      Author: Pillow sleeper
+ */
+
+
